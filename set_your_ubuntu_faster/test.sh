ca6761906b14cc67ab7f6c6f13e291a4a33713d5
Hector CORDAT-BOURSIAC <hector.cordat-boursiac@epitech.eu>
2025-02-06 19:05:32 +0100

    [DEL] need sudo to lunch

--- set_your_ubuntu_faster/test.sh
+++ set_your_ubuntu_faster/test.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo apt update
-sudo snap install multipass
 echo "penser a enlever la vm apres "
 multipass launch --cloud-init ./cloud-config.yaml --name test --cpus 10  --memory 8G
 ip=$(multipass list | grep test | awk '{print $3}')
