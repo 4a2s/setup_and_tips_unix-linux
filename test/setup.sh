b4d309e195194cf63cd93670bca4894d995ad0fb
Hector CORDAT-BOURSIAC <hector.cordat-boursiac@epitech.eu>
2025-02-06 14:22:04 +0100

    [FIX] discord

--- test/setup.sh
+++ test/setup.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo snap install multipass
 echo "penser a enlever la vm apres "
-multipass launch --cloud-init ./cloud-config.yaml --name test --cpus 10  --memory 12G
+multipass launch --cloud-init ./cloud-config.yaml --name test --cpus 10  --memory 8G
 multipass shell test
 
 
