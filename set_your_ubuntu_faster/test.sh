f2cb6cb19f69dc33c5508f7d552af7aa085a3422
antoine <antoine.picard@epfl.ch>
2025-02-06 15:02:14 +0100

    [FEAT] end of graphical vm

--- set_your_ubuntu_faster/test.sh
+++ set_your_ubuntu_faster/test.sh
@@ -2,6 +2,6 @@
 sudo apt update
 sudo snap install multipass
 echo "penser a enlever la vm apres "
-multipass launch --cloud-init ./cloud-config.yaml --name test
+multipass launch --cloud-init ./cloud-config.yaml --name test --cpus 10  --memory 8G
 ip=$(multipass list | grep test | awk '{print $3}')
 ssh -X test@$ip
